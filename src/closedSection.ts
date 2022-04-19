fa204fb6826ed43cfad34b2c9f43a05ff2093f66
penicillin0 <ayahitonakamura@gmail.com>
2022-04-19 16:01:25 +0900

    update

--- src/closedSection.ts
+++ src/closedSection.ts
@@ -3,4 +3,15 @@ export class ClosedSection {
     this.top = top;
     this.bottom = bottom;
   }
+
+  public getSectionString = () => {
+    return `[${this.top},${this.bottom}]`;
+  };
+
+  public has = (num: number) => {
+    if (this.top <= num && num <= this.bottom) {
+      return true;
+    }
+    return false;
+  };
 }
--- test/ClosedSection.spec.ts
+++ test/ClosedSection.spec.ts
@@ -12,8 +12,33 @@ describe("上端点と下端点を持つことができる", () => {
     it("上端点として 1 (number)が取得できる", () => {
       assert.strictEqual(cs.top, 1);
     });
-    it("下端点して3(number)が取得できる", () => {
+    it("下端点して 3 (number)が取得できる", () => {
       assert.strictEqual(cs.bottom, 3);
+    });
+  });
+
+  describe("[1, 3] の閉区間を文字列に変換できる", () => {
+    cs = new ClosedSection(1, 3);
+    it(" 1, 3 を渡すと`[1,3]`にできる", () => {
+      assert.strictEqual(cs.getSectionString(), "[1,3]");
+    });
+  });
+
+  describe("[1, 3] の閉区間で指定した整数を含むのか判定できる", () => {
+    beforeEach(() => {
+      cs = new ClosedSection(1, 3);
+    });
+
+    it("4 -> false を得る", () => {
+      assert.strictEqual(cs.has(4), false);
+    });
+
+    it("1 -> true を得る", () => {
+      assert.strictEqual(cs.has(1), true);
+    });
+
+    it("2 -> false を得る", () => {
+      assert.strictEqual(cs.has(2), true);
     });
   });
 });
